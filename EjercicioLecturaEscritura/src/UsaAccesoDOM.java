45cd8e342124f78064e87d1684b89513041045c9
Jorgenor56 <132491623+Jorgenor56@users.noreply.github.com>
2023-12-05 11:45:39 +0100

    Add files via upload

---
+++ EjercicioLecturaEscritura/src/UsaAccesoDOM.java
@@ -0,0 +1,27 @@
+import java.io.File;
+
+public class UsaAccesoDOM {
+    public static void main(String[] args) {
+        AccesoDOM accesoDOM = new AccesoDOM();
+        //Reemplaza con la ubicación de tu archivo XML
+        File archivoXML = new File("Libros.xml");
+
+        //Abrir el archivo XML
+        int resultadoApertura = accesoDOM.abrirXMLaDOM(archivoXML);
+        if (resultadoApertura == 0) {
+            //Mostrar el contenido del archivo XML
+            accesoDOM.recorreDOMyMuestra();
+
+            //Insertar un libro en el DOM
+            accesoDOM.insertarLibroEnDOM("Yerma", "Lorca", "1935");
+
+            //Borrar un nodo por título
+            accesoDOM.deleteNode("Don Quijote");
+
+            //Guardar el DOM modificado en un nuevo archivo
+            accesoDOM.guardarDOMcomoArchivo("LibrosDeDOM.xml");
+        }
+    }
+}
+
+
